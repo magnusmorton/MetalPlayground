64212ee7b965b6cbb2458d4a6f987affa2f8d710
Magnus Morton <magnusmorton@icloud.com>
2017-04-12 21:21:22 +0100

    Initial Commit

---
+++ MetalPlayground/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MetalPlayground
+//
+//  Created by John Morton on 12/04/2017.
+//  Copyright © 2017 Magnus. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
